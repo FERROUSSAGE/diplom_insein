acb639fb553dacfda2b09f0b785ee9af94e6719d
Ilya Uglovskoy <kaproman00@mail.ru>
2020-11-25 13:21:38 +0300

    feat privacy-policy.js: add modul

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import fromFooterToTop from './modules/footer';
 import telAccord from './modules/header-tel';
 import menu from './modules/menu';
 import { popupRepair } from './modules/popup-repair-types';
+import { popupPrivacy } from './modules/privacy-policy';
 
 telAccord();
 menu();
 fromFooterToTop();
 popupRepair();
+popupPrivacy();
---
+++ src/modules/privacy-policy.js
@@ -0,0 +1,16 @@
+const { popupRepair } = require("./popup-repair-types");
+
+const popup = () => {
+    const linkPrivacy = document.querySelectorAll('.link-privacy'),
+        popupPrivacy = document.querySelector('.popup-privacy'),
+        popupClose = popupPrivacy.querySelector('.close '); 
+
+    linkPrivacy.forEach((item) => {
+        item.addEventListener('click', () => popupPrivacy.style.visibility = 'visible');
+    });
+
+    popupClose.addEventListener('click', () => popupPrivacy.style.visibility = 'hidden');
+
+};
+
+export {popup as popupPrivacy};
