6f8959b3a560ee7b0742aaeed5aa3079879c9c40
Ilya Uglovskoy <kaproman00@mail.ru>
2020-11-25 17:42:38 +0300

    feat popup-consultation.js: new feat

---
+++ src/modules/popup-consultation.js
@@ -0,0 +1,12 @@
+const popup = () => {
+    const btns = document.querySelectorAll('.button_wide'),
+        popupConsultation = document.querySelector('.popup-consultation'),
+        btnClose = popupConsultation.querySelector('.close');
+
+    btns.forEach((item) => {
+        item.addEventListener('click', () => popupConsultation.style.visibility = 'visible');
+    }) 
+    btnClose.addEventListener('click', () => popupConsultation.style.visibility = 'hidden');
+};
+
+export {popup as popupConsultation};
